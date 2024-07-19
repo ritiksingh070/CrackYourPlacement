6f5d57578b746194352915fbed07075e9fd6285f
Ritik Singh <ritiksingh1809@gmail.com>
2024-07-19 23:39:46 +0530

    Day4

---
+++ Day4/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int start = prices[0];
+        int len = prices.length;
+        for(int i = 1;i<len; i++){
+            if(start < prices[i]) max += prices[i] - start;
+            start = prices[i];
+        }
+        return max;
+    }
+}
