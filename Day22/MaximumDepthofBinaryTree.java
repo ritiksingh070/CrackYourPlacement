0ed8c4078193706291594bd1d01be620b2db5693
Ritik Singh <ritiksingh1809@gmail.com>
2024-08-06 23:57:21 +0530

    Day22

---
+++ Day22/MaximumDepthofBinaryTree.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return  1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
